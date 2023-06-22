5b85035991eeb4598d7015601793444bb39460fa
Samantha Carvalho <samantha.s.carvalho@outlook.com>
2023-06-21 21:23:35 -0700

    cleanup: remove article section

--- src/data/user.js
+++ src/data/user.js
@@ -27,12 +27,6 @@ const INFO = {
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
 	},
 
-	articles: {
-		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-	},
-
 	projects: [
 		{
 			title: "Project 1",
